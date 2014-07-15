dbfafc6ffc263ceac6e9c881253f238e3133daf0
Falcon760 <sfalcon266@hotmail.com>
2014-07-15 07:23:20 -0400

    new

---
+++ CoinFlip/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    public class Coin
+    {
+        private static Random num = new Random();
+        public static void Flip(int times)
+        {
+            int heads = 0;
+            int tails = 0;
+            for (int i = 0; i < times; i++)
+            {
+                
+
+                if (num.Next(2) == 1)
+                {
+                    Console.WriteLine("HEADS");
+                    heads++;
+                }
+                else
+                {
+                    Console.WriteLine("TAILS");
+                    tails++;
+                }
+            }
+            Console.WriteLine("{0} instances of heads and {1} instances of tails.", heads, tails);
+            double percent1 = ((double)heads/(double)times);
+           double percent2 = ((double)tails/(double)times);
+            Console.WriteLine("This equates to {0:P2} percent of heads and {1:P2} percent of tails.", percent1, percent2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Coin.Flip(100);
+
+          
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s1 = "This C# is some easy stuff.";
+            //Console.WriteLine("The original sentence was {0} {1} {0}", Environment.NewLine, s1);
+            //string s2 = s1.Replace("easy", "hard");
+            //Console.WriteLine("The correct sentence is {0} {1} {0}", Environment.NewLine, s2);
+            //Console.ReadLine();
+            string e1 = "Eat at the Free Food Depot Tuesday night and get something free.";
+            int count = 0;    
+            foreach (char e in e1)
+                {
+                    if (e == 'e')
+                        count++;
+                }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
