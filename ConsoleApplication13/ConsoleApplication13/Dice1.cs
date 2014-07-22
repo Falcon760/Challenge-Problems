90446e022ae071030989132e50fa3e9f59ece629
Falcon760 <sfalcon266@hotmail.com>
2014-07-22 13:32:12 -0400

    new

---
+++ ConsoleApplication13/ConsoleApplication13/Dice1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Dice1
+    {
+
+        public int DieOne { get; set; }
+        public int DieTwo { get; set; }
+        List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6, };
+
+
+    }
+}
